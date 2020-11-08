3443788199fac18350c95e226ca4f9f5f3af4da8
Martin Timothy Timko <45509732+mttcode@users.noreply.github.com>
2020-11-08 16:23:36 +0100

    Create IUserRepository.cs

---
+++ server/Interfaces/IUserRepository.cs
@@ -0,0 +1,20 @@
+using NightRiders.DTOs;
+using NightRiders.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NightRiders.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Update(AppUser user);
+        Task<bool> SaveAllAsync();
+        Task<IEnumerable<AppUser>> GetUsersAsync();
+        Task<AppUser> GetUserByIdAsync(int id);
+        Task<AppUser> GetUserByUsernameAsync(string username);
+        Task<IEnumerable<MemberDto>> GetMembersAsync();
+        Task<MemberDto> GetMemberAsync(string username);
+    }
+}
