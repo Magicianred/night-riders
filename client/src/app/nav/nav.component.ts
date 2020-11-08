25fdc2cb61b01dc15d525053d0be7871604f1aa2
Martin Timothy Timko <45509732+mttcode@users.noreply.github.com>
2020-11-08 16:48:37 +0100

    Create nav.component.ts

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+
+export class NavComponent implements OnInit 
+{
+  model: any = {}
+
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) 
+  { 
+
+  }
+
+  ngOnInit(): void 
+  {
+
+  }
+
+  login() 
+  {
+    this.accountService.login(this.model).subscribe(response => {
+      this.router.navigateByUrl('/members');
+    })
+  }
+
+  logout() {
+    this.accountService.logout();
+    this.router.navigateByUrl('/');
+  }
+}
