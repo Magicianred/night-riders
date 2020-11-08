2cbaf87dce420fe7145e9f8c972bca358a96a688
Martin Timothy Timko <45509732+mttcode@users.noreply.github.com>
2020-11-08 16:17:40 +0100

    Create PhotoDto.cs

---
+++ server/DTOs/PhotoDto.cs
@@ -0,0 +1,9 @@
+namespace NightRiders.DTOs
+{
+    public class PhotoDto
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public bool IsMain { get; set; }
+    }
+}
