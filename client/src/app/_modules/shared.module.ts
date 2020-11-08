6c468475624f42975d9d8c5f0443ba23d4d9deaa
Martin Timothy Timko <45509732+mttcode@users.noreply.github.com>
2020-11-08 16:44:09 +0100

    Create shared.module.ts

---
+++ client/src/app/_modules/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ToastrModule } from 'ngx-toastr';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    }),
+    TabsModule.forRoot()
+  ],
+  exports: [
+    BsDropdownModule,
+    ToastrModule,
+    TabsModule
+  ]
+})
+export class SharedModule { }
