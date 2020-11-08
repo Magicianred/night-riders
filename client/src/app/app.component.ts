a7553f4dff652ca7880f39c688c9cda4cd5bf137
Martin Timothy Timko <45509732+mttcode@users.noreply.github.com>
2020-11-08 16:38:32 +0100

    Create app.component.ts

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit 
+{
+  title = 'Night Riders';
+  users: any;
+
+  constructor(private accountService: AccountService){}  
+
+  ngOnInit() 
+  {
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
+  }
+}
