093865e9e30b2b710c73cdc89b4fd4e50909b7c6
Martin Timothy Timko <45509732+mttcode@users.noreply.github.com>
2020-11-08 16:09:12 +0100

    Create BaseApiController.cs

---
+++ server/Controllers/BaseApiController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NightRiders.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+
+
+
+
+    }
+}
