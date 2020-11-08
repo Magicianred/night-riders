7d6a57e760eb2797f8dbba8e6db0c565cb16564f
Martin Timothy Timko <45509732+mttcode@users.noreply.github.com>
2020-11-08 16:15:24 +0100

    Create Seed.cs

---
+++ server/Data/Seed.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using NightRiders.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace NightRiders.Data
+{
+    public class Seed
+    {
+        public static async Task SeedUsers(DataContext context)
+        {
+            if (await context.Users.AnyAsync()) return;
+
+            var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
+            var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+
+            foreach(var user in users)
+            {
+                using var hmac = new HMACSHA512();
+
+                user.UserName = user.UserName.ToLower();
+                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("f7spXfcknGTi84a"));
+                user.PasswordSalt = hmac.Key;
+
+                context.Users.Add(user);
+            }
+
+            await context.SaveChangesAsync();
+
+        }
+    }
+}
