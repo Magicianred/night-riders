4736550f8bdf2e38ca19ca07e4229df5523ed2c7
Martin Timothy Timko <45509732+mttcode@users.noreply.github.com>
2020-11-08 16:15:44 +0100

    Create UserRepository.cs

---
+++ server/Data/UserRepository.cs
@@ -0,0 +1,70 @@
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using NightRiders.DTOs;
+using NightRiders.Entities;
+using NightRiders.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace NightRiders.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext context;
+        private readonly IMapper mapper;
+
+        public UserRepository(DataContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public async Task<IEnumerable<AppUser>> GetUsersAsync()
+        {
+            return await context.Users
+                .Include(p => p.Photos)
+                .ToListAsync();
+        }
+
+        public async Task<AppUser> GetUserByIdAsync(int id)
+        {
+            return await context.Users.FindAsync(id);
+        }
+
+        public async Task<AppUser> GetUserByUsernameAsync(string username)
+        {
+            return await context.Users
+                .Include(p => p.Photos)
+                .SingleOrDefaultAsync(x => x.UserName == username);
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await context.SaveChangesAsync() > 0;
+        }
+
+        public void Update(AppUser user)
+        {
+            context.Entry(user).State = EntityState.Modified;
+        }
+
+        public async Task<IEnumerable<MemberDto>> GetMembersAsync()
+        {
+            return await context.Users
+                .ProjectTo<MemberDto>(mapper.ConfigurationProvider)
+                .ToListAsync();
+        }
+
+        public async Task<MemberDto> GetMemberAsync(string username)
+        {
+            return await context.Users
+                .Where(x => x.UserName == username)
+                .ProjectTo<MemberDto>(mapper.ConfigurationProvider)
+                .SingleOrDefaultAsync();
+        }
+    }
+}
