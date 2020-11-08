06a6e300e97c2c12f7131ba8607fffd7f8b575ed
Martin Timothy Timko <45509732+mttcode@users.noreply.github.com>
2020-11-08 16:10:00 +0100

    Create BuggyController.cs

---
+++ server/Controllers/BuggyController.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NightRiders.Data;
+using NightRiders.Entities;
+
+namespace NightRiders.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly DataContext context;
+
+        public BuggyController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "secret text";
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var thing = context.Users.Find(-1);
+
+            if (thing == null) return NotFound();
+
+            return Ok(thing);
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+            var thing = context.Users.Find(-1);
+
+            if (thing == null) return NotFound();
+
+            return thing.ToString();
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("This was not a good request");
+        }
+    }
+}
