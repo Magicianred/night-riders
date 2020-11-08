6b928069ead902bbc5911661a182630257f3920d
Martin Timothy Timko <45509732+mttcode@users.noreply.github.com>
2020-11-08 16:51:25 +0100

    Create member-card.component.ts

---
+++ client/src/app/members/member-card/member-card.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
+
+@Component({
+  selector: 'app-member-card',
+  templateUrl: './member-card.component.html',
+  styleUrls: ['./member-card.component.css']
+})
+export class MemberCardComponent implements OnInit {
+  @Input() member: Member;
+
+  constructor(private memberService: MembersService, private toastr: ToastrService) { }
+
+  ngOnInit(): void {
+  }
+
+  addLike(member: Member) {
+    this.memberService.addLike(member.username).subscribe(() => {
+      this.toastr.success('You have liked ' + member.knownAs);
+    })
+  }
+
+}
